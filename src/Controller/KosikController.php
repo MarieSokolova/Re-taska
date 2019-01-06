95165bd1c1b365f1d4a454803cfd4597a4d4e654
Marie <>
2019-01-06 12:17:35 +0000

    kosik

--- src/Controller/KosikController.php
+++ src/Controller/KosikController.php
@@ -9,33 +9,60 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
+/**
+ * @Route("/kosik")
+ */
 class KosikController extends AbstractController
 {
     /**
-     * @Route("/kosik/{id}", name="vlozeni_produktu")
+     * @Route("/", name="kosik", methods="GET|POST")
      */
     
-public function insert(SessionInterface $session, Product $product): Response
+    public function view(SessionInterface $session): Response
     {
+     $kosik = $session->get('kosik', []);
+     return $this->render('kosik/index.html.twig',  ['kosik' => $kosik]);
+    }
+    
+    
+    /**
+     * @Route("/{id}", name="vlozeni_produktu", methods="GET|POST")
+     */
+    
+    public function insert(SessionInterface $session, Product $product): Response
+    {
+        //$session->set('kosik', []);
         // načtení počtu ze session (defaultní hodnota je 0)
         $kosik = $session->get('kosik', []);
-        $kosik[] = ['name'=>$product->getNazev(), 'cena'=>$product->getCena(), 'pocet'=>1];
+        $kosik[] = ['nazev'=>$product->getNazev(), 'cena'=>$product->getCena(), 'pocet'=>1];
         
               
         // uložení nové hodnoty do session
         $session->set('kosik', $kosik);
         
         // vykreslení šablony
-        $this->redirect('kosik');
+        return $this->redirectToRoute('kosik');
     }
+   
     /**
-     * @Route("/kosik", name="kosik")
+     * @Route("/{id}/edit", name="kosik_edit", methods="GET|POST")
      */
+    public function edit(SessionInterface $session, Request $request, Product $product): Response
+    {
+        $form = $this->createForm(KosikType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('kosik', ['id' => $product->getId()]);
+        }
+
+        return $this->render('kosik/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    } 
     
-public function view(SessionInterface $session): Response
-{
-     $kosik = $session->get('kosik', []);
-     $this->render('kosik/index.html.twig',  ['kosik' => $kosik]);
-}
   
 }
